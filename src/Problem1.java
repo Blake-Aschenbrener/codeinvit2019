343787b5489fb915bd55a401670c3eda8193de45
Blake Aschenbrener <33679428+Blake-Aschenbrener@users.noreply.github.com>
2019-09-29 13:16:41 -0400

    Update Problem1.java

--- src/Problem1.java
+++ src/Problem1.java
@@ -1,2 +1,32 @@
+/**
+After increasing tuition yet again, President Barbara Snyder decided to use excess revenue on coloring the binary walkway. After coloring the binary walkway, it will become a long rectangle made of  colored blocks. President Snyder has  colors at her disposal. Highly valuing diversity, President Synder does not want to any two adjacent blocks to have the same color. Could you calculate how many ways President Snyder could color the binary walkway? Because the number of ways can be very large, we only need you to print the remainder of the answer with  .
+
+Input Format
+
+The input contains one line, with two non-negative integers  ,  denotes the number of columns of the pathway and the number of colors Babs wants to use.
+
+Constraints
+
+, 
+
+Output Format
+
+The output only on one line, is an interger denotes the answer.
+
+Sample Input 0
+
+0 1
+Sample Output 0
+
+1
+Sample Input 1
+
+4 16
+Sample Output 1
+
+791285303
+**/
 public class Problem1 {
+  
+  
 }
